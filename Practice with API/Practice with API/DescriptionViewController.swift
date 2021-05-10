56afdfee2436c0b70508fe94162100caf80a22e8
Grigory <grishavinyar@icloud.com>
2021-05-10 11:49:54 +0300

    Добавил DescriptionViewController

---
+++ Practice with API/Practice with API/DescriptionViewController.swift
@@ -0,0 +1,19 @@
+//
+//  DescriptionViewController.swift
+//  Practice with API
+//
+//  Created by Григорий Виняр on 10.05.2021.
+//
+
+import UIKit
+
+class DescriptionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+
+}
